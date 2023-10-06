8795a13d7fdcaedce6b5e1256ffa96fe4314b020
SHAH HUZAIFA <143290124+shahhuzaifa@users.noreply.github.com>
2023-10-06 15:10:11 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,28 @@
+
+// var booleanArray = [true, false, true, false, true];
+// console.log(booleanArray);
+
+
+// var stringsArray = ["apple", "banana", "orange", "grape"];
+// console.log(stringsArray);
+
+
+
+var numbersArray = [1, 2, 3, 4, 5];
+console.log(numbersArray);
+
+var store = [ "SSC", "HSC"," BCS","BS", "BCOM", "MS", "M. Phil","phd"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
